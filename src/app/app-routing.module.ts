8d48b4c0f36c9dd16d5a0288156b5346e5943657
BuildTools <mattieu.vendeville@gmail.com>
2020-09-24 18:21:37 +0200

    Updating routes by adding HomeComponent as Index

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CategoriesComponent } from './categories/categories.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  {path: '', component: HomeComponent},
   {path: 'categories', component: CategoriesComponent}
 ];
 
