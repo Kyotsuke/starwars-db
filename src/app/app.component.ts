2a6093ffae84cb007ad7de314dfa5019bcaa47a4
BuildTools <mattieu.vendeville@gmail.com>
2020-09-25 18:05:35 +0200

    Create a function to get all Categories

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'starwars-db';
+}
+
+
+export class SWAPI {
+  api_url = 'https://swapi.dev/api/'
+  categories = [];
+  peoples = [];
+
+  constructor(private http: HttpClient) {}
+
+  getCategories() {
+    let http = HttpClient;
+    this.http.get(this.api_url).toPromise().then(data => {
+      for (let property in data) {
+        let category = {
+          name: "",
+          url: ""
+        }
+        if (data.hasOwnProperty(property)) {
+          category.name = property;
+          category.url = data[property];
+          this.categories.push(category);
+        }
+      }
+    })
+
+    return this.categories;
+  }
 }
