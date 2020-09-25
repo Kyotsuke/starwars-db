2466e97a69c49f571e3f3494507accb9e89fe780
BuildTools <mattieu.vendeville@gmail.com>
2020-09-25 14:33:35 +0200

    Import SWAPI in Home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  api_url = 'https://swapi.dev/api/'
+  items = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.http.get(this.api_url).toPromise().then(data => {
+      console.log(data);
+
+      for (let property in data) {
+        let category = {
+          name: "",
+          url: ""
+        }
+        if (data.hasOwnProperty(property)) {
+          category.name = property;
+          category.url = data[property];
+          this.items.push(category);
+        }
+      }
+    })
+  }
 
   ngOnInit(): void {
   }
