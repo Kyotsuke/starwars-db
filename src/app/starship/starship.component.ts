ce4da90250ac5c5c3f0accd6f6eb52b7e11421be
BuildTools <mattieu.vendeville@gmail.com>
2020-09-28 23:03:33 +0200

    Initiate Starship component with is function

--- src/app/starship/starship.component.ts
+++ src/app/starship/starship.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { ActivatedRoute } from '@angular/router/'
+
+import { SWAPI } from '../app.component';
 
 @Component({
   selector: 'app-starship',
@@ -6,10 +11,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./starship.component.scss']
 })
 export class StarshipComponent implements OnInit {
+  constructor(private http: HttpClient, private route: ActivatedRoute) {}
 
-  constructor() { }
+  swapi = new SWAPI(this.http);
+  starship_id = parseInt(this.route.snapshot.paramMap.get('id'));
+  starship = this.swapi.getStarship(this.starship_id, false);
 
   ngOnInit(): void {
+    console.log(this.starship);
   }
 
 }
