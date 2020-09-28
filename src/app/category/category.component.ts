ec793c0545a011b13c39f67784b8a677755ea492
BuildTools <mattieu.vendeville@gmail.com>
2020-09-28 15:59:33 +0200

    Display Category for each link in router

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Router, NavigationEnd } from '@angular/router';
+
+import { SWAPI } from '../app.component';
 
 @Component({
   selector: 'app-category',
@@ -7,9 +12,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) {}
+
+  swapi = new SWAPI(this.http);
+  elements = [];
+  category_name = "";
 
   ngOnInit(): void {
+
+    if (this.router.url === "/people"){
+      this.elements = this.swapi.getPeoples();
+      this.category_name = "Peoples"
+    }
+
+    if (this.router.url === "/planets"){
+      this.elements = this.swapi.getPlanets();
+      this.category_name = "Planets"
+    }
+
+    if (this.router.url === "/films"){
+      this.elements = this.swapi.getFilms();
+      this.category_name = "Films"
+    }
+
+    if (this.router.url === "/species"){
+      this.elements = this.swapi.getSpecies();
+      this.category_name = "Species"
+    }
+
+    if (this.router.url === "/vehicles"){
+      this.elements = this.swapi.getVehicles();
+      this.category_name = "Vehicles"
+    }
+
+    if (this.router.url === "/starships"){
+      this.elements = this.swapi.getStarships();
+      this.category_name = "Starships"
+    }
   }
 
 }
