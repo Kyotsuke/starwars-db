b4eb8dfd08d20c2be2aa0e76c9198531fd58cd32
BuildTools <mattieu.vendeville@gmail.com>
2020-09-28 15:15:21 +0200

    Creating new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,27 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { PeopleComponent } from './people/people.component';
+import { PlanetComponent } from './planet/planet.component';
+import { FilmComponent } from './film/film.component';
+import { SpecieComponent } from './specie/specie.component';
+import { VehicleComponent } from './vehicle/vehicle.component';
+import { StarshipComponent } from './starship/starship.component';
+import { CategoryComponent } from './category/category.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    PeopleComponent,
+    PlanetComponent,
+    FilmComponent,
+    SpecieComponent,
+    VehicleComponent,
+    StarshipComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/specie/specie.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-specie',
+  templateUrl: './specie.component.html',
+  styleUrls: ['./specie.component.scss']
+})
+export class SpecieComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
