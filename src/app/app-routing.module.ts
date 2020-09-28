2c5a5b5a6a609fa22afc252a84eb9fa32798e479
BuildTools <mattieu.vendeville@gmail.com>
2020-09-28 15:15:28 +0200

    Making new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,31 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
+import { CategoryComponent } from './category/category.component';
+
+import { PeopleComponent } from './people/people.component';
+import { PlanetComponent } from './planet/planet.component';
+import { FilmComponent } from './film/film.component';
+import { SpecieComponent } from './specie/specie.component';
+import { VehicleComponent } from './vehicle/vehicle.component';
+import { StarshipComponent } from './starship/starship.component';
+
 const routes: Routes = [
   {path: '', component: HomeComponent},
+
+  {path: 'people', component: CategoryComponent},
+  {path: 'planets', component: CategoryComponent},
+  {path: 'films', component: CategoryComponent},
+  {path: 'species', component: CategoryComponent},
+  {path: 'vehicles', component: CategoryComponent},
+  {path: 'starships', component: CategoryComponent},
+
+  {path: 'people/:id', component: PeopleComponent},
+  {path: 'planets/:id', component: PlanetComponent},
+  {path: 'films/:id', component: FilmComponent},
+  {path: 'species/:id', component: SpecieComponent},
+  {path: 'vehicles/:id', component: VehicleComponent},
+  {path: 'starships/:id', component: StarshipComponent},
 ];
 
 @NgModule({
