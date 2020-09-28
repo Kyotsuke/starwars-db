ed458717d45d5991feb6dfad156134fce66ab176
BuildTools <mattieu.vendeville@gmail.com>
2020-09-28 22:01:11 +0200

    Update Category component

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { Router, NavigationEnd } from '@angular/router';
+import { Router } from '@angular/router';
 
 import { SWAPI } from '../app.component';
 
