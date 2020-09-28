952cffb39a53823d388795ff02d02e2b0e26d32e
BuildTools <mattieu.vendeville@gmail.com>
2020-09-28 22:02:12 +0200

    Create & display one character by his id

--- src/app/people/people.component.ts
+++ src/app/people/people.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { ActivatedRoute } from '@angular/router/'
+
+import { SWAPI } from '../app.component';
+
 
 @Component({
   selector: 'app-people',
@@ -6,8 +12,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./people.component.scss']
 })
 export class PeopleComponent implements OnInit {
+  constructor(private http: HttpClient, private route: ActivatedRoute) {}
 
-  constructor() { }
+  swapi = new SWAPI(this.http);
+  people_id = parseInt(this.route.snapshot.paramMap.get('id'));
+  people = this.swapi.getPeople(this.people_id);
+  films = [];
+  vehicles = [];
+  starships = []
 
   ngOnInit(): void {
   }
