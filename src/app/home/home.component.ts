8fce1756f402c2a233690ee17d96c5a05a1b8897
BuildTools <mattieu.vendeville@gmail.com>
2020-09-28 15:17:05 +0200

    Update on Home component links

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+
 import { SWAPI } from '../app.component';
 
 @Component({
@@ -12,26 +13,6 @@ export class HomeComponent implements OnInit {
 
   swapi = new SWAPI(this.http);
   categories = this.swapi.getCategories();
-  // api_url = 'https://swapi.dev/api/'
-  // items = [];
-
-  // constructor(private http: HttpClient) {
-  //   this.http.get(this.api_url).toPromise().then(data => {
-  //     console.log(data);
-
-  //     for (let property in data) {
-  //       let category = {
-  //         name: "",
-  //         url: ""
-  //       }
-  //       if (data.hasOwnProperty(property)) {
-  //         category.name = property;
-  //         category.url = data[property];
-  //         this.items.push(category);
-  //       }
-  //     }
-  //   })
-  // }
 
   ngOnInit(): void {
   }
