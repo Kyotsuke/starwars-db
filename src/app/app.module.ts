a9428e295896d8f1bc3e9544d3fe4e9b9ae6c9f4
BuildTools <mattieu.vendeville@gmail.com>
2020-09-29 17:01:21 +0200

    Create a Search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { SpecieComponent } from './specie/specie.component';
 import { VehicleComponent } from './vehicle/vehicle.component';
 import { StarshipComponent } from './starship/starship.component';
 import { CategoryComponent } from './category/category.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { CategoryComponent } from './category/category.component';
     SpecieComponent,
     VehicleComponent,
     StarshipComponent,
-    CategoryComponent
+    CategoryComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
