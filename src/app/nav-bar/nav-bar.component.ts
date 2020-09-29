d008bf932f29a907432e0c5d74d532a003861301
BuildTools <mattieu.vendeville@gmail.com>
2020-09-29 17:01:55 +0200

    Add a search bar in Nav component

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -8,6 +8,7 @@ import { Component, HostBinding, OnInit } from '@angular/core';
 export class NavBarComponent implements OnInit {
 
   isOpened: boolean = false;
+  search_value = "";
 
   constructor() { }
 
@@ -16,6 +17,11 @@ export class NavBarComponent implements OnInit {
 
   NavStatus(status: boolean) {
     this.isOpened = status;
+  }
+
+  getVal(item) {
+    console.log(item.target.value);
+    this.search_value = item.target.value;
   }
 
 }
