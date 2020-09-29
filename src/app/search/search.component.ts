8a3a4f84bf2534c1afb0032194195a81d0b7650a
BuildTools <mattieu.vendeville@gmail.com>
2020-09-29 18:44:48 +0200

    Make Search component functional to search data in SWAPI

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { ActivatedRoute } from '@angular/router/'
+
+import { SWAPI } from '../app.component';
+
 
 @Component({
   selector: 'app-search',
@@ -6,10 +12,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
+  constructor(private http: HttpClient, private route: ActivatedRoute) {}
 
-  constructor() { }
+  swapi = new SWAPI(this.http);
+  search_value = this.route.snapshot.paramMap.get('search_value');
+  search = this.swapi.search(this.search_value);
 
   ngOnInit(): void {
+    console.log(this.search);
   }
 
 }
